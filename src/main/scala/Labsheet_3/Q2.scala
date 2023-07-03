6e8915976f5729276a10f3bbfa93c13eb28cdb77
Inuka Weerasekera <inuka@16-Inukas-MacBook-Pro.local>
2023-07-03 10:28:08 +0530

    small change

--- src/main/scala/Labsheet_3/Q2.scala
+++ src/main/scala/Labsheet_3/Q2.scala
@@ -7,8 +7,8 @@ object Q2 {
   def filstr(x: List[String]): List[String] = x.filter(_.length > 5)
 
   def main(args: Array[String]): Unit ={
-    val input= List("mercedes", "volvo", "jaguar", "mini", "porsche")
-    val out = filstr(input)
+    val x= List("mercedes", "volvo", "jaguar", "mini", "porsche")
+    val out = filstr(x)
     printf("The strings which are greater than 5 are %s",out)
 
   }
